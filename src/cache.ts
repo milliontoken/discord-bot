45346d1a16806b1cfda609f0e062fd8d0acbf20a
Peter Tran <tran.peter08@gmail.com>
2021-07-29 16:05:29 -0700

    create Cache class

--- src/cache.ts
+++ src/cache.ts
@@ -1,8 +1,27 @@
 import * as NodeCache from 'node-cache';
 
-const cacheOptions: NodeCache.Options = {
-  stdTTL: 10, // keys live for 10 seconds
-  useClones: true,
-};
+class Cache {
+  cache: NodeCache;
+  constructor() {
+    const cacheOptions: NodeCache.Options = {
+      stdTTL: 10, // keys live for 10 seconds
+      useClones: true,
+    };
 
-export const cache = new NodeCache(cacheOptions);
+    this.cache = new NodeCache(cacheOptions);
+  }
+
+  get(key: string) {
+    return this.cache.get(key);
+  }
+
+  set(key: string, value: unknown, ttl?: string | number) {
+    return this.cache.set(key, value, ttl);
+  }
+
+  has(key: string) {
+    return this.cache.has(key);
+  }
+}
+
+export const cache = new Cache();
