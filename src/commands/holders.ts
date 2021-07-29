741655cad93cab011051e81092a63587e59af55a
Peter Tran <tran.peter08@gmail.com>
2021-07-29 05:22:31 -0700

    updated holders to include BSC holders

--- src/commands/holders.ts
+++ src/commands/holders.ts
@@ -14,16 +14,38 @@ module.exports = class HelloCommand extends SlashCommand {
   }
 
   async run(ctx) {
-
-    const apiUrl = `https://api.ethplorer.io/getAddressInfo/0x6b4c7a5e3f0b99fcd83e9c089bddd6c7fce5c611?apiKey=freekey`;
+    const ethExplorerUrl = `https://api.ethplorer.io/getAddressInfo/0x6b4c7a5e3f0b99fcd83e9c089bddd6c7fce5c611?apiKey=freekey`;
+    const covalentUrl = `https://api.covalenthq.com/v1/56/tokens/0xBF05279F9Bf1CE69bBFEd670813b7e431142Afa4/token_holders/?key=${process.env.COVALENT_API_KEY}&page-size=1`;
     const init = {
       headers: {
         'content-type': 'application/json;charset=UTF-8',
       },
     };
-    const response = await fetch(apiUrl, init);
-    const responseBody = await response.json();
-    await ctx.send(`<:pepeholdmm:861835461458657331> Current holders count is **${responseBody.tokenInfo.holdersCount}**.`);
 
+    try {
+      const [ethExplorerResp, covalentResp]: Response[] = await Promise.all([
+        fetch(ethExplorerUrl, init),
+        fetch(covalentUrl, init),
+      ]);
+
+      const [ethExplorerBody, covalentBody] = await Promise.all([
+        ethExplorerResp.json(),
+        covalentResp.json(),
+      ]);
+
+      const numFormatter = new Intl.NumberFormat('en-US');
+
+      const holders = numFormatter.format(
+        ethExplorerBody.tokenInfo.holdersCount +
+          covalentBody.data.pagination.total_count,
+      );
+
+      await ctx.send(
+        `<:pepeholdmm:861835461458657331> Current holders count is **${holders}**.`,
+      );
+    } catch (error) {
+      console.log(error);
+      await ctx.send(`Something went wrong - try again a bit later.`);
+    }
   }
 };
